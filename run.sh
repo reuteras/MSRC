c356e9e6109a835197169a9a2c93beef8ff827bb
Peter Reuterås <peter@reuteras.com>
2021-07-14 07:29:26 +0200

    Switch to Microsoft registry for Docker container.

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm -it --name powershell -e APIKEY="$(cat api.txt)" -v "$(pwd)":/mnt microsoft/powershell pwsh /mnt/GenMSRCDocs.ps1
+docker run --rm -it --name powershell -e APIKEY="$(cat api.txt)" -v "$(pwd)":/mnt mcr.microsoft.com/powershell pwsh /mnt/GenMSRCDocs.ps1
